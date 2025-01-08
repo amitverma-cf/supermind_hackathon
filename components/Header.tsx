6ea10e50391ea57b8d84d36cc04325307c62f85a
amitverma-cf <ava.amitverma@gmail.com>
2025-01-09 04:13:12 +0530

    update logo and favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,13 +6,13 @@ import {
     MenubarSeparator,
     MenubarTrigger,
 } from "@/components/ui/menubar"
-import { AlignJustify, ArrowUpRight, Download, University } from "lucide-react"
+import { AlignJustify, ArrowUpRight, Clover, Download } from "lucide-react"
 import Link from "next/link"
 
 export function Header() {
     return (
         <Menubar className="fixed bottom-0 lg:bottom-8 w-full lg:left-1/3 lg:w-fit mx-auto inset-x-0 h-14 lg:border-2 gap-4 px-4 shadow-xl border-black justify-between lg:justify-evenly rounded-none lg:rounded-md dark:border-white z-50 transition-transform">
-            <University className="text-red-500 -mt-1" />
+            <Clover className="text-red-500 -mt-1" />
             <div className={`hidden md:flex gap-6`}>
                 <MenubarMenu>
                     <Link href={"https://github.com/amitverma-cf/supermind_hackathon"}>Github <ArrowUpRight className="inline transition-opacity" /></Link>
